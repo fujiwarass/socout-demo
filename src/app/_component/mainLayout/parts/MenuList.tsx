f77eff2d11a7f1d1e6d3ba4ebd589c74f01a7575
fujisann1024 <tehepero302@gmail.com>
2024-09-01 03:04:07 +0900

    リンク先の付与

--- src/app/_component/mainLayout/parts/MenuList.tsx
+++ src/app/_component/mainLayout/parts/MenuList.tsx
@@ -1,32 +1,48 @@
 import { AttachMoneyOutlined, ContactPhoneOutlined, HomeOutlined, ModeCommentOutlined } from '@mui/icons-material';
 import { Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material';
+import Link from 'next/link';
+
+/**
+ * メニューの型定義
+ */
+interface menuItem {
+  /** メニュー名 */
+  text: string,
+  /** メニューのパス */
+  path: string,
+  /** メニューのアイコン */
+  icon: React.ComponentType,
+}
 
 /**
  * サイドバーに表示するメニュー一覧
  * (メニュー名: アイコン)
  */
 export function MenuList() {
-    const MenuListItem : Record<string, JSX.Element> = {
-        ホーム: <HomeOutlined fontSize='small'/>,
-        スカウト一覧: <ContactPhoneOutlined fontSize='small'/>, 
-        メッセージ一覧: <ModeCommentOutlined fontSize='small'/>, 
-        チケット: <AttachMoneyOutlined fontSize='small'/>
-       };
+    //メニューの一覧
+    const MenuListItem :  menuItem[]= [
+      {text: "ホーム", path: "/", icon: HomeOutlined},
+      {text: "スカウト一覧", path: "", icon: ContactPhoneOutlined},
+      {text: "メッセージ一覧", path: "", icon: ModeCommentOutlined},
+      {text: "チケット", path: "", icon: AttachMoneyOutlined}
+    ]
     
     return (
         <>
            <Toolbar />
            <Divider />
            <List>
-           {['ホーム', 'スカウト一覧', 'メッセージ一覧', 'チケット'].map((text) => (
-               <ListItem key={text} disablePadding>
+           {MenuListItem.map((item,index) => (
+              <Link href={item.path} key={index}>
+               <ListItem key={index} disablePadding>
                  <ListItemButton>
                    <ListItemIcon>
-                   {MenuListItem[text]}
+                     <item.icon />
                    </ListItemIcon>
-                   <ListItemText primary={text} />
+                   <ListItemText primary={item.text} />
                  </ListItemButton>
                </ListItem>
+              </Link>
              ))}
            </List>
         </>
